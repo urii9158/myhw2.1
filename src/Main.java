67feac1d43a31d88b0123d9718cb19ad49ea199f
uri <katyacitti@gmail.com>
2024-03-02 16:08:39 +0200

    version2

--- src/Main.java
+++ src/Main.java
@@ -2,9 +2,10 @@ public class Main {
     public static void main(String[] args) {
 
         int price = 13_676;
-        int miles = price / 20;
+        int miles = 20;
+        int bonus = price / 20;
 
-        System.out.println("Бонус:" + miles);
+        System.out.println("Бонус: " + bonus);
 
     }
 }
