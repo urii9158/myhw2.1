169a8720c8ee459e393c9c7c4e0aca39b40925f8
uri <katyacitti@gmail.com>
2024-03-02 16:02:04 +0200

    add

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13_676;
+        int miles = price / 20;
+
+        System.out.println("Бонус:" + miles);
+
+    }
+}
